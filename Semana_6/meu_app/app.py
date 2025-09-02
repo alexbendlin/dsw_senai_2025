b266b8c57619db513584729225ca3a137aa89ce5
Alexandre Bendlin <133797103+alexbendlin@users.noreply.github.com>
2025-09-01 21:09:28 -0300

    Semana 6 - Desafio

--- Semana_6/meu_app/app.py
+++ Semana_6/meu_app/app.py
@@ -8,7 +8,7 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Sua-Segurança-Mora-Aqui'
 
-app.config['SQLACHEMY_DATABASE_URI'] = 'sqlite:///meuapp.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///meuapp.db'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -23,6 +23,14 @@ class Usuario(db.Model):
 
     def __repr__(self):
         return f'<Usuário: {self.nome}>'
+
+class Postagem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    titulo = db.Column(db.String(80), unique=True, nullable=False)
+    descricao = db.Column(db.String(200), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<Título: {self.titulo}>'
 
 
 # --- Rotas da Aplicação ---
@@ -45,6 +53,25 @@ def adicionar_usuario():
     db.session.commit()
     
     return redirect(url_for('index'))
+
+@app.route('/postagens')
+def postagens():
+    postagens = Postagem().query.all()
+    return render_template('postagens.html', postagens=postagens)
+
+@app.route('/adicionar_postagem', methods=['POST'])
+def adicionar_postagem():
+    titulo = request.form['titulo']
+    descricao = request.form['descricao']
+    
+    nova_postagem = Postagem(titulo=titulo, descricao=descricao)
+    
+    db.session.add(nova_postagem)
+    
+    db.session.commit()
+    
+    return redirect(url_for('postagens'))
+
 
 # Passo 3: Criando o Banco de Dados Físico
 if __name__ == '__main__':
